be041a472d42f4313f44b8dd641072b5dad4d45f
Matthew Yang <yang70@gmail.com>
2015-09-08 14:04:27 -0700

    refactor fizzbuzzadvanced a bit

--- fizzbuzz_advanced.rb
+++ fizzbuzz_advanced.rb
@@ -1,22 +1,23 @@
+# Input is an array of hashes where the key is the replacement word and
+# the value is the number divisible bye to return the word.
 def fizzbuzzadvanced(hashes_array, limit)
-  (0..limit).each do |number|
-    string = ''
+  (0..limit).each do |num|
+    str = ''
     hashes_array.each do |hash|
       hash.each do |key, value|
-        if number % value == 0
-          string += key
-        end
+        str += key if num % value == 0
       end
     end
 
-    if string.empty?
-      puts number
+    if str.empty? #can't make this a ternary, for some reason?
+      puts num
     else
-      puts string
+      puts str
     end
   end
 end
 
+# Test case
 customer_data = []
 customer_data[0] = { "fizz" => 3 }
 customer_data[1] = { "buzz" => 5 }
