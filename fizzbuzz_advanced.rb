7b63f890dfb6d3f10ae611abe1bafd754032cdaf
Matthew Yang <yang70@gmail.com>
2015-09-08 14:17:47 -0700

    update test cases

--- fizzbuzz_advanced.rb
+++ fizzbuzz_advanced.rb
@@ -19,9 +19,8 @@ end
 
 # Test case
 customer_data = []
-customer_data[0] = { "fizz" => 3 }
-customer_data[1] = { "buzz" => 5 }
+customer_data[0] = { "Fizz" => 3 }
+customer_data[1] = { "Buzz" => 5 }
 customer_data[2] = { "Sivv" => 7 }
-customer_data[3] = { "Grrr" => 13}
 
-fizzbuzzadvanced(customer_data, 100)
+fizzbuzzadvanced(customer_data, 105)
